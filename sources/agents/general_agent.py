4a09c6056b25a05bc05c5920b2d85f26b2fcec73
cid <cid@ciddebijibendiannao.local>
2026-01-26 15:44:17 +0800

    optimization

--- sources/agents/general_agent.py
+++ sources/agents/general_agent.py
@@ -373,6 +373,8 @@ class GeneralAgent(Agent):
                         else:
                             raise ValueError(f"Unsupported HTTP method: {method}")
 
+                        self.logger.info(f"request query: {request_params}")
+                        self.logger.info(f"request body: {request_body}")
                         self.logger.info(f"Response body: {response.text}")
                         # 返回响应结果
                         return response.json() if response.content else None
